b059e1bcade95cbd18dfe40ef9c4bb49dad80c9c
metarth <krystian.jasionek@protonmail.com>
2026-08-14 16:27:10 +0200

    configure base to the root

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vitePluginString from "vite-plugin-string";
 
 export default defineConfig({
+  base: "/",
   plugins: [
     vitePluginString({
       include: ["**/*.frag", "**/*.vert"],
